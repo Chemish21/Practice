e6d3c26d0380ea3c1f0fd9f4dbc32afc2fa07563
Sam Morris <smorris3131@gmail.com>
2026-01-18 22:52:12 -0500

    adding multiple movies to database

--- .vscode/Udemy/Mongoose_Demo/index.js
+++ .vscode/Udemy/Mongoose_Demo/index.js
@@ -18,4 +18,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/test')
   });
 
   const Movie = mongoose.model('Movie', movieSchema)
-  const amadeus = new Movie({title: "Amadeus", year: 1986, score: 9.2, rating: 'R'})
+  //const amadeus = new Movie({title: "Amadeus", year: 1986, score: 9.2, rating: 'R'})
+
+  Movie.insertMany([
+    {title: 'Amelie', year: 2001, score: 8.3, rating: 'R'},
+    {title: 'Alien', year: 1979, score: 8.1, rating: 'R'},
+    {title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG'},
+    {title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R'},
+    {title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13'}
+  ])
+    .then(data => {
+      console.log('It Worked!');
+      console.log(data);
+    })
